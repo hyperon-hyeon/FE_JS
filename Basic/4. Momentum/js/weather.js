af253f38205b7f7a0ea3c2164def606ad73a5a10
hyperon-hyeon <hyperon1122@gmail.com>
2025-05-15 19:24:54 +0900

    Momentum - Weather

---
+++ Basic/4. Momentum/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEYS="c2e333028e122a8f322fae19365307cd";
+function onGeoOk(position){
+    const lat=position.coords.latitude;
+    const lon=position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS}&units=metric&lang=kr`
+    fetch(url).then(response => response.json())
+    .then((data)=>{
+        const weather=document.querySelector("#weather span:first-child");
+        const city=document.querySelector("#weather span:last-child");
+        city.innerText=data.name;
+        weather.innerText=`${data.weather[0].main}/${data.main.temp} degree\n`;
+    });
+}
+
+function onGeoErr(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoErr);
